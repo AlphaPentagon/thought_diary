0a3c70b4ef04bf3082f968c26361438b33c534b3
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-09-07 16:08:26 +0100

    Created logic for all backend routes, separated into controller file

---
+++ backend/controllers/thoughtController.js
@@ -0,0 +1,168 @@
+import thoughtModel from "../models/thoughtModel.js";
+import mongoose from "mongoose";
+
+// get all thoughts
+export const getAllThoughts = async (req, res) => {
+  try {
+    const allThoughts = await thoughtModel.find({}).sort({ createdAt: -1 });
+    res.status(200).json({
+      message: "GET all thoughts",
+      success: true,
+      payload: allThoughts,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      payload: null,
+    });
+  }
+};
+
+// get a single thought
+export const getThoughtById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    const thought = await thoughtModel.find({ _id: id });
+
+    console.log(thought);
+    if (thought.length === 0) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    res.status(200).json({
+      message: `GET thought by id of ${id}`,
+      success: true,
+      payload: thought,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      payload: null,
+    });
+  }
+};
+
+// create a new thought
+export const createThought = async (req, res) => {
+  const {
+    situation,
+    thought,
+    thought_rating,
+    emotions,
+    behaviours,
+    evidence_for,
+    evidence_against,
+    balanced_thought,
+    balanced_rating,
+  } = req.body;
+
+  // add thought to database
+  try {
+    const newThought = await thoughtModel.create({
+      situation,
+      thought,
+      thought_rating,
+      emotions,
+      behaviours,
+      evidence_for,
+      evidence_against,
+      balanced_thought,
+      balanced_rating,
+    });
+    res.status(201).json({
+      message: "POSTED new thought",
+      success: true,
+      payload: newThought,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      payload: null,
+    });
+  }
+};
+
+// delete a thought
+export const deleteThoughtByID = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    const deletedThought = await thoughtModel.findOneAndDelete({ _id: id });
+    if (!deletedThought) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    res.status(200).json({
+      message: `DELETED thought with id of ${id}`,
+      success: true,
+      payload: deletedThought,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      payload: null,
+    });
+  }
+};
+
+// update a thought
+
+export const updateThoughtByID = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    const updatedThought = await thoughtModel.findOneAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      }
+    );
+    if (!updatedThought) {
+      return res.status(400).json({
+        message: `No such thought with id of ${id}`,
+        success: false,
+        payload: [],
+      });
+    }
+    res.status(200).json({
+      message: `UPDATED thought with id of ${id}`,
+      success: true,
+      payload: updatedThought,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      payload: null,
+    });
+  }
+};
--- backend/routes/thoughts.js
+++ backend/routes/thoughts.js
@@ -1,79 +1,27 @@
 import express from "express";
-import thoughtModel from "../models/thoughtModel.js";
+import {
+  createThought,
+  deleteThoughtByID,
+  getAllThoughts,
+  getThoughtById,
+  updateThoughtByID,
+} from "../controllers/thoughtController.js";
 
 const router = express.Router();
 
 // GET all thoughts
-router.get("/", (req, res) => {
-  res.status(200);
-  res.json({ status: res.status, success: true, payload: "GET all thoughts" });
-});
+router.get("/", getAllThoughts);
 
 // GET a single thought
-router.get("/:id", (req, res) => {
-  res.status(200);
-  res.json({
-    success: true,
-    payload: "GET single thought by id",
-  });
-});
+router.get("/:id", getThoughtById);
 
 // POST a new thought
-router.post("/", async (req, res) => {
-  const {
-    situation,
-    thought,
-    thought_rating,
-    emotions,
-    behaviours,
-    evidence_for,
-    evidence_against,
-    balanced_thought,
-    balanced_rating,
-  } = req.body;
-
-  try {
-    const newThought = await thoughtModel.create({
-      situation,
-      thought,
-      thought_rating,
-      emotions,
-      behaviours,
-      evidence_for,
-      evidence_against,
-      balanced_thought,
-      balanced_rating,
-    });
-    res.status(201).json({
-      message: "POST new thought",
-      success: true,
-      payload: newThought,
-    });
-  } catch (error) {
-    res.status(400).json({
-      message: error.message,
-      success: false,
-      payload: null,
-    });
-  }
-});
+router.post("/", createThought);
 
 // DELETE a thought
-router.delete("/:id", (req, res) => {
-  res.status(200);
-  res.json({
-    success: true,
-    payload: "DELETE a single thought",
-  });
-});
+router.delete("/:id", deleteThoughtByID);
 
 // PATCH a thought
-router.patch("/:id", (req, res) => {
-  res.status(200);
-  res.json({
-    success: true,
-    payload: "UPDATE a single thought",
-  });
-});
+router.patch("/:id", updateThoughtByID);
 
 export default router;
