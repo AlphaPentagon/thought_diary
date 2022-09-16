8aefbafc22823bcca70df291b26c73e8f3e41f2c
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-09-16 13:12:45 +0100

    Added redirects file

--- frontend/src/components/ThoughtForm/ThoughtForm.js
+++ frontend/src/components/ThoughtForm/ThoughtForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import TextForm from "./TextForm/TextForm";
 import NumberForm from "./NumberForm/NumberForm";
