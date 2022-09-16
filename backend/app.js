847951d585c3e829c3686752a3a0dc880f20198c
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-09-16 13:34:16 +0100

    enabling CORS...again

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 // middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method, req.body);
